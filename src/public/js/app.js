4e8b1769e33bdaf8b8ee08b3ae358a960859d849
mmtakahashi <m.takahashi@gftd.works>
2022-01-21 14:12:06 +0900

    Clean up the folder

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2024,36 +2024,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   components: {
--- src/resources/js/components/NfrashCardsComponent.vue
+++ src/resources/js/components/NfrashCardsComponent.vue
@@ -3,36 +3,6 @@
         <div class="text-wrapper">
             <p>{{arryCheck()}}</p>
             <h2>{{QAcheck}}</h2>
-
-            <!-- <h2 class="text" v-if="isActive === 1">{{ arrayElements() }}</h2>
-            <h2 class="text" v-else>ちがうよ</h2> -->
-
-            <!-- <h2 class="text" v-if="isActive === 1">{{ tasks[0].question }}</h2>
-
-            <h2 class="text" v-else-if="isActive === 2">{{ tasks[0].answer }}</h2>
-            <h2 class="text" v-else-if="isActive === 3">{{ tasks[1].question }}</h2>
-            <h2 class="text" v-else-if="isActive === 4">{{ tasks[1].answer }}</h2>
-            <h2 class="text" v-else-if="isActive === 5">{{ tasks[2].question }}</h2>
-            <h2 class="text" v-else-if="isActive === 6">{{ tasks[2].answer }}</h2>
-            <h2 class="text" v-else-if="isActive === 7">{{ tasks[3].question }}</h2>
-            <h2 class="text" v-else-if="isActive === 8">{{ tasks[3].answer }}</h2>
-            <h2 class="text" v-else-if="isActive === 9">{{ tasks[4].question }}</h2>
-            <h2 class="text" v-else-if="isActive === 10">{{ tasks[4].answer }}</h2>
-            <h2 class="text" v-else-if="isActive === 11">{{ tasks[5].question }}</h2>
-            <h2 class="text" v-else-if="isActive === 12">{{ tasks[5].answer }}</h2>
-            <h2 class="text" v-else-if="isActive === 13">{{ tasks[6].question }}</h2>
-            <h2 class="text" v-else-if="isActive === 14">{{ tasks[6].answer }}</h2>
-            <h2 class="text" v-else-if="isActive === 15">{{ tasks[7].question }}</h2>
-            <h2 class="text" v-else-if="isActive === 16">{{ tasks[7].answer }}</h2>
-            <h2 class="text" v-else-if="isActive === 17">{{ tasks[8].question }}</h2>
-            <h2 class="text" v-else-if="isActive === 18">{{ tasks[8].answer }}</h2>
-            <h2 class="text" v-else-if="isActive === 19">{{ tasks[9].question }}</h2>
-            <h2 class="text" v-else-if="isActive === 20">{{ tasks[9].answer }}</h2>
-            <h2 class="text" v-else-if="isActive === 21">{{ tasks[10].question }}</h2>
-            <h2 class="text" v-else-if="isActive === 22">{{ tasks[10].answer }}</h2>
-            
-            <h2 class="text" v-else>{{ tasks[10].answer }}</h2> -->
-
         </div>
 
         <div class="button-container">
